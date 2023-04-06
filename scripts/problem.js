f988dfd99d5bf186cb2c4d492795541f247474a5
dhruva-shashi <dhruva.shashi@gmail.com>
2023-04-06 17:06:04 +0530

    best-score-fix-1

--- scripts/problem.js
+++ scripts/problem.js
@@ -50,6 +50,67 @@ function init_cookie() {
 	document.cookie = cookie_content;
 }
 
+function update_cookie(id, results) {
+	// Obtain problems.json as a JSON object names "problems"
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", 'https://uvce-hash-code-hub.github.io/practice-website/problems.json', false);
+	xmlHttp.send(null);
+
+	var problems = JSON.parse(xmlHttp.responseText);
+
+	if (document.cookie == '') {
+		var cookie_content = 'problem-scores=';
+		var problems_and_scores = {}
+
+		for (problem in problems) {
+			var number_of_inputs = problems[problem]['number-of-inputs'];
+			var scores = []
+
+			// Number of 0s appended is equal to the number of inputs for the problem
+			for (var i = 0; i < number_of_inputs; i++)
+				scores.push(0);
+
+			problems_and_scores[problem] = scores;
+		}
+
+		cookie_content += JSON.stringify(problems_and_scores);
+		cookie_content += "; expires=Tue, 31 Dec 2029 23:59:59 UTC";
+
+		document.cookie = cookie_content;
+	}
+
+	var cookie_content = 'problem-scores=';
+	var problems_and_scores = JSON.parse(document.cookie.split(';')[0].split('=')[1]);
+
+	for (problem in problems) {
+		// Check for problems whose score is not present in the cookie and initialize the cookie again
+		if (!(problem in problems_and_scores)) {
+			var number_of_inputs = problems[problem]['number-of-inputs'];
+			var scores = [];
+
+			// Number of 0s appended is equal to the number of inputs for the problem
+			for (var i = 0; i < number_of_inputs; i++)
+				scores.push(0);
+
+			problems_and_scores[problem] = scores;
+		}
+		if (problem == id) {
+			var number_of_inputs = problems[problem]['number-of-inputs'];
+			var scores = [];
+
+			for (var i = 0; i < number_of_inputs; i++)
+				scores.push(max(problems_and_scores[problem][i], results[i]['points']));
+
+			problems_and_scores[problem] = scores;
+		}
+	}
+
+	cookie_content += JSON.stringify(problems_and_scores);
+	cookie_content += "; expires=Tue, 31 Dec 2029 23:59:59 UTC";
+
+	document.cookie = cookie_content;
+}
+
 function init() {
 	// Obtain problems.json as a JSON object names "problems"
 	var xmlHttp = new XMLHttpRequest();
@@ -172,6 +233,10 @@ function evaluate_problem(id) {
 	var files = [];
 	var file_readers = [];
 
+	var results = {};
+
+	var error_message = '';
+
 	for (let i = 0; i < n; i++) {
 		files[i] = document.getElementById(`file${i+1}`).files[0];
 		file_readers[i] = new FileReader();
@@ -189,13 +254,20 @@ function evaluate_problem(id) {
 
 				// Obtain the output score for the submission
 				result = evaluate_score(id, input_content, output_content);
-				alert(JSON.stringify(result));
+
+				if ('error' in result)
+					error_message += `${String.fromCharCode(i+65)}: ${result['error']}\n`;
+				else
+					results[i] = result;
 			}
 
 			file_readers[i].readAsText(files[i]);
 			document.getElementById(`file${i+1}`).value = "";
 		}
 	}
+
+	update_cookie(id, results);
+	init();
 }
 
 
